379d5dc38dbcccab66efddb891314bb4a68ae0ca
Gajendra Singh <133628620+gajendra07singh@users.noreply.github.com>
2024-05-24 21:01:07 +0530

    Add files via upload

---
+++ ATM-interface-with-JAVA/ATM/src/atm/ATM.java
@@ -0,0 +1,10 @@
+package atm;
+public class ATM {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Login().setVisible(true);
+    }
+
+}
---
+++ ATM-interface-with-JAVA/README.md
@@ -0,0 +1,4 @@
+# ATM-interface-with-JAVA-GUI
+
+https://user-images.githubusercontent.com/76885324/170088231-41861ccf-43ab-4bb3-8cfd-ca2e85144e05.mp4
+
